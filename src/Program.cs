b77a630fac4f169125f24a521cb20281bfe4f58c
d1zainer <kiviplus1@mail.ru>
2024-11-27 22:01:32 +0300

    update

--- src/Program.cs
+++ src/Program.cs
@@ -8,12 +8,9 @@ namespace QR_API
 {
     public class Program
     {
-        private const string _commandTest = "http --url=helpful-orca-worthy.ngrok-free.app 5053";
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            string desktopPath = Path.Combine(Environment.CurrentDirectory, "ngrok.exe");
-            RunExternalApp(desktopPath, _commandTest);
-
+            
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddSwaggerGen(c =>
             {
@@ -61,26 +58,7 @@ namespace QR_API
             // Запускаем приложение
             app.Run();
         }
-        private static void RunExternalApp(string filePath, string arguments)
-        {
-            try
-            {
-                // Настраиваем процесс для запуска внешнего приложения
-                var processStartInfo = new ProcessStartInfo
-                {
-                    FileName = filePath, // Указанный путь к ngrok.exe
-                    Arguments = arguments, // Аргументы для ngrok
-                    UseShellExecute = true, // Позволяет запускать приложение в отдельной консоли
-                    CreateNoWindow = false // Создает отдельное окно для приложения
-                };
-                // Запускаем процесс
-                Process.Start(processStartInfo);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Failed to run external app: " + ex.Message);
-            }
-        }
+      
     }
 }
 
